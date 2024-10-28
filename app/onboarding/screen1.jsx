0c2c96bcfaed6af276c0ec7fd19148e0fb68c1c9
Kofi Akyea <kofiaakyea@gmail.com>
2024-10-28 15:26:00 +0000

    first screen

---
+++ app/onboarding/screen1.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+const Screen1 = () => {
+  return (
+    <View>
+      <Text>Screen 1</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Screen1;
---
+++ constants/images.js
@@ -0,0 +1,5 @@
+import logo from "../assets/images/logo.png";
+import people from "../assets/images/people-head.png";
+import wave from "../assets/images/wave.png";
+
+export default { logo, people, wave };
